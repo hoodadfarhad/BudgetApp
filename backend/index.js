2fd046dedc8c12a0cf603c15ef402c766de03773
Hoodad <hoodad.farhad@gmail.com>
2025-05-15 18:37:58 -0400

    using .Env file for storing more data, enabling to work both on localhost and EC2 instance

--- backend/index.js
+++ backend/index.js
@@ -35,8 +35,8 @@ app.use('/auth', authRoutes);
 
 const db = new pg.Client({
   user: process.env.DB_USER,
-  host: "localhost",
-  database: "BudgetApp",
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port:5432
 })
